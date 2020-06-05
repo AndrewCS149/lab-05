dad404c6f305e018ee096f1dcaaa20f9fcb7c1a6
AndrewCS149 <andrewcs149@gmail.com>
2020-06-05 16:45:48 -0700

    RMake copy of public folder and delete public folder.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 require('dotenv').config();
 
 // hey express, find the public folder, and serve files from there
-app.use(express.static('./public'));
+app.use(express.static('./portfolio'));
 
 const PORT = process.env.PORT;
 
